ad862e983ac6a0bde479aaa2e1dc129cd5a36529
raghavsharma007 <princeraghav.sharma@gmail.com>
2021-06-18 18:43:53 +0530

    updated urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from rest_framework_simplejwt.views import TokenObtainPairView
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView 
